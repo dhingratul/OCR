427a297916500e5b343bd14386c616d107e03092
dhingratul <atul.dhingra@rutgers.edu>
2016-02-19 12:26:46 -0500

    Added morphological features

--- BoundingBox.m
+++ BoundingBox.m
@@ -1,6 +1,6 @@
 % L is the connected components obtained from L=bwlabel(im2), where im2 is
 % binarized
-function [Features]=BoundingBox(L,im2,disp_on_off,AR_threshold)
+function [Features,cim]=BoundingBox(L,im2,disp_on_off,AR_threshold,R_Size,C_Size,AR,Pixel_No, Neighborhood)
 Features=[];
 Nc=max(max(L));
 % figure
@@ -13,11 +13,18 @@ maxr=max(r);
 minr=min(r);
 maxc=max(c);
 minc=min(c);
-if(minr~=maxr && minc~=maxc && (maxr-minr)-(maxc-minc)<AR_threshold)
-rectangle('Position',[minc,minr,maxc-minc+1,maxr-minr+1], 'EdgeColor','w');
+if(minr~=maxr && minc~=maxc && abs((maxr-minr)-(maxc-minc))<AR_threshold && abs((maxr-minr)>R_Size) && abs((maxc-minc)>C_Size)&& abs((maxr-minr)/(maxc-minc))<AR )
 cim = im2((minr+1)-1:(maxr+1)+1,(minc+1)-1:(maxc+1)+1);
+if(sum(cim(:))>Pixel_No)
+rectangle('Position',[minc,minr,maxc-minc+1,maxr-minr+1], 'EdgeColor','w');
 [centroid, theta, roundness, inmo] = moments(cim, disp_on_off);
-Features=[Features;theta,roundness,inmo];
+area=sum(cim(:));
+% hogfeature = extractHOGFeatures(cim)
+% perimeter=bwperim(cim,Neighborhood); perimeter=sum(perimeter(:))
+rmv = bwmorph(cim,'remove');
+skeleton=bwmorph(rmv,'skel',Inf); skeleton=sum(skeleton(:));
+Features=[Features;theta,roundness,inmo,area,centroid,skeleton];%,perimeter,hogfeature];
+end
 end
 end
 hold off
--- OCR_Extract_Features.m
+++ OCR_Extract_Features.m
@@ -1,43 +1,51 @@
 % function[train,labeltr,mu,sigma]=OCR_Extract_Features(im,thr)
 clear all;close all; clc;
-load data;
+% load data;
 fea=[];count=0;
-thr=200;
-% srcFiles = dir('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/*.bmp');
-%     for i=1:length(srcFiles)
-%         filename = strcat('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/',srcFiles(i).name);
-%         im =imread(filename);
-%         [C]=strsplit(srcFiles(i).name,'.');
-%         label=double(C{1,1})-96;
-%% Testing
-addpath('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/Test')
-im=imread('test2.bmp');
-%%
-        im2=im; im2=uint8(im<thr);
+thr=205;
+srcFiles = dir('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/*.bmp');
+    for i=1:length(srcFiles)
+        filename = strcat('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/',srcFiles(i).name);
+        im =imread(filename);
+        [C]=strsplit(srcFiles(i).name,'.');
+        label=double(C{1,1})-96;
+% %% Testing
+% addpath('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/Test')
+% im=imread('test1.bmp');
+%%      
+     
+        im2=im; imd=uint8(im<thr);
+           se = strel('disk',1,4);
+        im2d=imdilate(imd,se);
+        se = strel('disk',1,4);
+        im2=imerode(im2d,se);
+        
         L=bwlabel(im2);
-        temp=BoundingBox(L,im2,0,80);
+        [temp,cim]=BoundingBox(L,im2,0,50,5,5,10,85,8);
         if(i==1)
             fea=temp;
-%             labeltr(1:size(temp,1),:)=label;
+            labeltr(1:size(temp,1),:)=label;
             
         else
         fea=[fea;temp];
-%         for k=length(labeltr)+1:length(labeltr)+size(temp,1)
-%             labeltr(k,1)=label;
-%         end
+ %% Labelling Data
+        for k=length(labeltr)+1:length(labeltr)+size(temp,1)
+            labeltr(k,1)=label;
+        end
         temp=0;
         end
-%     end
+    end
 %% Normalization of features
 for k=1:size(fea,2)
-%     mu(1,k)=mean(fea(:,k));
-%     sigma(1,k)=var(fea(:,k));
+    mu(1,k)=mean(fea(:,k));
+    sigma(1,k)=var(fea(:,k));
     fea(:,k)=(fea(:,k)-mu(1,k))/sigma(1,k);
 end
+% fea=normc(fea);
 %% Confusion Matrix Calculation
-d=dist2(train,fea);
+d=dist2(fea,fea);
 [D_sorted, D_index]=sort(d,1);
-label_new=D_index(1,:);
+label_new=D_index(2,:)';
 
 for i=1:size(D_index,2)
     label_conf(i,1)=labeltr(label_new(i));
