a875dab4639494eef9b8d69837913e8ce11a4e60
dhingratul <atul.dhingra@rutgers.edu>
2016-02-20 13:45:42 -0500

    Recogntiion accuracy at 32% and 42%

--- OCR_Extract_Features.m
+++ OCR_Extract_Features.m
@@ -1,17 +1,17 @@
 % function[train,labeltr,mu,sigma]=OCR_Extract_Features(im,thr)
 clear all;close all; clc;
-% load data;
+load data; load labels;
 fea=[];count=0;
-thr=205;
-srcFiles = dir('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/*.bmp');
-    for i=1:length(srcFiles)
-        filename = strcat('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/',srcFiles(i).name);
-        im =imread(filename);
-        [C]=strsplit(srcFiles(i).name,'.');
-        label=double(C{1,1})-96;
-% %% Testing
-% addpath('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/Test')
-% im=imread('test1.bmp');
+thr=200;
+% srcFiles = dir('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/*.bmp');
+%     for i=1:length(srcFiles)
+%         filename = strcat('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/',srcFiles(i).name);
+%         im =imread(filename);
+%         [C]=strsplit(srcFiles(i).name,'.');
+%         label=double(C{1,1})-96;
+%% Testing
+addpath(genpath('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code/H1-16images/Test'))
+im=imread('test2.bmp');
 %%      
      
         im2=im; imd=uint8(im<thr);
@@ -24,17 +24,17 @@ srcFiles = dir('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1/Code
         [temp,cim]=BoundingBox(L,im2,0,50,5,5,10,85,8);
         if(i==1)
             fea=temp;
-            labeltr(1:size(temp,1),:)=label;
+%             labeltr(1:size(temp,1),:)=label;
             
         else
         fea=[fea;temp];
- %% Labelling Data
-        for k=length(labeltr)+1:length(labeltr)+size(temp,1)
-            labeltr(k,1)=label;
+%  %% Labelling Data
+%         for k=length(labeltr)+1:length(labeltr)+size(temp,1)
+%             labeltr(k,1)=label;
+%         end
+%         temp=0;
         end
-        temp=0;
-        end
-    end
+%     end
 %% Normalization of features
 for k=1:size(fea,2)
     mu(1,k)=mean(fea(:,k));
@@ -43,16 +43,16 @@ for k=1:size(fea,2)
 end
 % fea=normc(fea);
 %% Confusion Matrix Calculation
-d=dist2(fea,fea);
+d=dist2(train,fea);
 [D_sorted, D_index]=sort(d,1);
-label_new=D_index(2,:)';
+label_new=D_index(1,:)';
 
 for i=1:size(D_index,2)
     label_conf(i,1)=labeltr(label_new(i));
 end
-%% Percentage accuracy
+%% Percentage accuracy - Recognition
 for i=1:size(D_index,2)
-if(label_conf(i)==labeltr(i))
+if(label_conf(i)==label2(i))
 count=count+1
 end
 end
