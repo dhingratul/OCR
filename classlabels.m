5b425c972a69ad37156ea49e422717b9d77a1f21
dhingratul <atul.dhingra@rutgers.edu>
2017-06-15 16:04:20 -0400

    Added Documentation

--- classlabels.m
+++ classlabels.m
@@ -1,15 +1,10 @@
+%% Returns testlabels based on locations and classes
 function [labelte]=classlabels(locations,classes,centroid)
-    if nargin<1
-        im=imread('test1.bmp');
-        load labels;
-        labelte=label1;
-    end
 
-    
- for i=1:length(classes)
-residual(i,1)=centroid(1)-locations(i,1);
-residual(i,2)=centroid(2)-locations(i,2);
- end
+for i=1:length(classes)
+    residual(i,1)=centroid(1)-locations(i,1);
+    residual(i,2)=centroid(2)-locations(i,2);
+end
 idx2 = knnsearch(residual(:,2),0);
 idx1 = knnsearch(residual(:,1),0);
 labelte=classes(idx2);
