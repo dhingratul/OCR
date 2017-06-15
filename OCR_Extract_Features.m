ea56080b071805bc2083c8f572bf6ece4651e0ed
dhingratul <atul.dhingra@rutgers.edu>
2017-06-15 15:58:01 -0400

    Indendted code

--- OCR_Extract_Features.m
+++ OCR_Extract_Features.m
@@ -16,34 +16,34 @@ thr=200;i=0;
 %         [C]=strsplit(srcFiles(i).name,'.');
 %         label=double(C{1,1})-96;
 
-%%      
-     
-        im2=im; imd=uint8(im<thr);
-           se = strel('disk',1,4);
-        im2d=imdilate(imd,se);
-        se = strel('disk',1,4);
-        im2=imerode(im2d,se);
-        
-        L=bwlabel(im2);
-        [temp,cim,labelte,L,im2]=BoundingBox(L,im2,0,50,5,5,10,85,8,locations,classes);
-        if(i==1)
-            fea=temp;
-%             labeltr(1:size(temp,1),:)=label;
-            
-        else
-        fea=[fea;temp];
-%  %% Automatic Labelling Data for training images
-%         for k=length(labeltr)+1:length(labeltr)+size(temp,1)
-%             labeltr(k,1)=label;
-%         end
-%         temp=0;
-        end
+%%
+
+im2=im; imd=uint8(im<thr);
+se = strel('disk',1,4);
+im2d=imdilate(imd,se);
+se = strel('disk',1,4);
+im2=imerode(im2d,se);
+
+L=bwlabel(im2);
+[temp,cim,labelte,L,im2]=BoundingBox(L,im2,0,50,5,5,10,85,8,locations,classes);
+if(i==1)
+    fea=temp;
+    %             labeltr(1:size(temp,1),:)=label;
+    
+else
+    fea=[fea;temp];
+    %  %% Automatic Labelling Data for training images
+    %         for k=length(labeltr)+1:length(labeltr)+size(temp,1)
+    %             labeltr(k,1)=label;
+    %         end
+    %         temp=0;
+end
 %     end
 %% Normalization of features
 load data4 mu sigma
 for k=1:size(fea,2)
-%     mu(1,k)=mean(fea(:,k));
-%     sigma(1,k)=var(fea(:,k));
+    %     mu(1,k)=mean(fea(:,k));
+    %     sigma(1,k)=var(fea(:,k));
     fea(:,k)=(fea(:,k)-mu(1,k))/sigma(1,k);
 end
 % fea=normc(fea);
@@ -60,9 +60,9 @@ end
 
 %% Percentage accuracy - Training
 for i=1:size(D_index,2)
-if(label_conf(i)==labeltr(i))
-count=count+1;
-end
+    if(label_conf(i)==labeltr(i))
+        count=count+1;
+    end
 end
 pertr=count/size(D_index,2)*100
 count=0;
@@ -78,9 +78,9 @@ end
 
 %% Percentage accuracy - Recognition
 for i=1:length(labelte)
-if(label_conf(i)==labelte(i))
-count=count+1;
-end
+    if(label_conf(i)==labelte(i))
+        count=count+1;
+    end
 end
 perte=count/size(D_index,2)*100;
 
