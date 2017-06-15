4e72e9700c29e633d103d4288273757dd7c5d92b
dhingratul <atul.dhingra@rutgers.edu>
2017-06-15 15:57:31 -0400

    Indendted code

--- RunMyOCRRecognition.m
+++ RunMyOCRRecognition.m
@@ -1,14 +1,12 @@
-%% If no input is provided, it runs by default on test1.bmp and its corresponding label that was designed by hand.
-function[perte,pertr] =RunMyOCRRecognition(testimage,locations, classes)
-clear all;close all;clc;
-if nargin<1
-load locations;
-locations=locations1;
-classes=classes1;
-addpath(genpath('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1'));
-testimage=imread('test1.bmp');
-end
+%% If no input is provided, it runs by default on test2.bmp and its corresponding label from locations, classes vectors
+function[perte,pertr,outlabelte] =RunMyOCRRecognition(testimage,locations, classes)
 %% Add path to where the source code for moments is located
-addpath(genpath('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1'));
-[perte,pertr]=OCR_Extract_Features(testimage,locations,classes);
+if nargin<1
+    addpath(genpath('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW1'));
+    load locations2;
+    im=imread('test2.bmp');
+    locations=locations2;
+    classes=classes2;
+end
+[perte,pertr,outlabelte]=OCR_Extract_Features(testimage,locations,classes);
 end
